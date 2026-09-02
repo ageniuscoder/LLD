8406db2c42a2333d2ee2fa13ee0f7ad818733d60
ageniuscoder <23bcs059@nith.ac.in>
2026-09-03 01:58:26 +0530

    rw lock from scratch

--- concurrencyPattern/readerwriter/main.go
+++ concurrencyPattern/readerwriter/main.go
@@ -2,62 +2,90 @@
 
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
-type RWLock struct{
-	mu sync.Mutex
-	cond *sync.Cond
+type RWLock struct {
+	mu     sync.Mutex
+	cond   *sync.Cond
 	reader int
-	writer int
-	readerreq int
+	writer bool
 }
 
-func NewRWLock() *RWLock{
-	rw:=&RWLock{}
-	rw.cond=sync.NewCond(&rw.mu)
+func NewRWLock() *RWLock {
+	rw := &RWLock{}
+	rw.cond = sync.NewCond(&rw.mu)
 	return rw
 }
 
-func (rw *RWLock) Rlock(){
+func (rw *RWLock) Rlock() {
 	rw.mu.Lock()
 	defer rw.mu.Unlock()
-	rw.readerreq++
-	for rw.writer>0 {
+	for rw.writer {
 		rw.cond.Wait()
 	}
 	rw.reader++
-	rw.readerreq--
 }
 
-func (rw *RWLock) Runlock(){
+func (rw *RWLock) Runlock() {
 	rw.mu.Lock()
 	defer rw.mu.Unlock()
 
 	rw.reader--
-	if rw.reader==0{
-		rw.cond.Signal()
-	}
-}
-
-func (rw *RWLock) Wlock(){
-	rw.mu.Lock()
-	defer rw.mu.Unlock()
-
-	for rw.reader>0 || rw.writer>0 || rw.readerreq>0{
-		rw.cond.Wait()
-	}
-
-	rw.writer++
-}
-
-func (rw *RWLock) Wunlock(){
-	rw.mu.Lock()
-	defer rw.mu.Unlock()
-
-	rw.writer--
-
-	if rw.writer==0{
+	if rw.reader == 0 {
 		rw.cond.Broadcast()
 	}
 }
-func main() {}
+
+func (rw *RWLock) Wlock() {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+
+	for rw.reader > 0 || rw.writer {
+		rw.cond.Wait()
+	}
+
+	rw.writer = true
+}
+
+func (rw *RWLock) Wunlock() {
+	rw.mu.Lock()
+	defer rw.mu.Unlock()
+
+	rw.writer = false
+	rw.cond.Broadcast()
+
+}
+func main() {
+	lock := NewRWLock()
+	var wg sync.WaitGroup
+
+	// Start 5 readers
+	for i := 1; i <= 10; i++ {
+		wg.Add(1)
+		go func(id int) {
+			defer wg.Done()
+			lock.Rlock()
+			fmt.Printf("Reader %d acquired lock\n", id)
+			time.Sleep(3 * time.Nanosecond)
+			fmt.Printf("Reader %d releasing lock\n", id)
+			lock.Runlock()
+		}(i)
+	}
+
+	// Start 1 writer
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		lock.Wlock()
+		fmt.Println("Writer acquired lock")
+		time.Sleep(100 * time.Millisecond)
+		fmt.Println("Writer releasing lock")
+		lock.Wunlock()
+	}()
+
+	wg.Wait()
+}
